405846bb3a3b5555d28bdf6b527798c17f7c50f4
Quinn <quinn.trang@pm.me>
2020-07-31 16:21:08 -0700

    Handle user input in text field

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,19 +8,48 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
     
-
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+    }
+    
+    
+    //text of textField will also be set if the user hits return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        //confirm that user is done editing -> dismiss keyboard
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //clear text field once search is entered
+        searchTextField.text = ""
+    }
+    
+    //check if user actually types in something
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        }
+        else {
+            searchTextField.placeholder = "Type something"
+            return false
+        }
+    }
 }
 
