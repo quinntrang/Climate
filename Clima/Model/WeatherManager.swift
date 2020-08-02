62990a6a84bd78461e1b0df3add4a6dba6f8e78b
Quinn <quinn.trang@pm.me>
2020-08-02 09:07:34 -0700

    Use URLSession to fetch data from Open Weather

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,11 +9,35 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=5dc08abdd11bdd6e86d9b93d0d4136a9&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=5dc08abdd11bdd6e86d9b93d0d4136a9&units=metric"
     
     func fetchWeather (cityName: String) {
-        let urlString = weatherURL + "&q=" + cityName
-        print(urlString)
+        //let urlString = weatherURL + "&q=" + cityName
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1.Creat URL
+        if let url = URL (string: urlString) {
+            //URL Session
+            let session = URLSession (configuration: .default)
+            //task
+            let task = session.dataTask(with: url, completionHandler: handle (data: response: error:))
+            //start task
+            task.resume()
+        }
+    }
+    
+    func handle (data: Data?, response: URLResponse?, error: Error?) {
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
         
+        if error != nil {
+            print(error!)
+            return
+        }
     }
 }
